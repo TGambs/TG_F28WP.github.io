aeedf6956c2b26adaf56bfcbe484d5c87a9d5cb7
Tommaso Gambro <tg2016@hw.ac.uk>
2023-10-09 09:52:06 +0100

    09/10/23 (1)
    Started on the cookies and live variables

--- java.js
+++ java.js
@@ -5,3 +5,14 @@ function changeColour(){
     let ranNum = Math.floor(Math.random() * 5);
     document.getElementById("colourButton").style.backgroundColor = colours[ranNum];
 }
+
+function isCookiesOn(){
+    var text = "aren't";
+    if (navigator.cookieEnabled == True){
+        text = "are";
+    }
+    alert("Cookies are enabled, press to continue")
+    document.getElementById("extraCookie").innerHTML = "Cookies" + text + "enabled"
+
+    
+}
