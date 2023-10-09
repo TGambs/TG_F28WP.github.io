4ed2eb7a5d8c8ba4f8188a22b4d5b3cdbfa70373
Tommaso Gambro <tg2016@hw.ac.uk>
2023-10-09 16:38:45 +0100

    09/10/23 (2)
    Improved layout, design and cookie functionality

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 # Created 15/09/23
 
 All Tasks and other materials can be found at https://f28wp.github.io
+
+On this website there is:
+- A button which cycles background colour when clicked
+- Cookie data
--- java.js
+++ java.js
@@ -6,13 +6,40 @@ function changeColour(){
     document.getElementById("colourButton").style.backgroundColor = colours[ranNum];
 }
 
-function isCookiesOn(){
-    var text = "aren't";
-    if (navigator.cookieEnabled == True){
+function cookieData(){
+    /*let text = "aren't";
+    if (navigator.cookieEnabled == true){
         text = "are";
-    }
-    alert("Cookies are enabled, press to continue")
-    document.getElementById("extraCookie").innerHTML = "Cookies" + text + "enabled"
+    } 
 
-    
+    alert("Cookies " +text+ " enabled, press to continue"); */
+
+    let usersName = prompt("Please enter your name:");
+
+    let day = new Date();
+    day = day.toUTCString();
+
+    document.getElementById("usersName").innerHTML = "Name: " + usersName;
+    document.getElementById("dateNow").innerHTML = "Current Date: " + day;
+    document.getElementById("extraCookie").innerHTML = "Cookies " + text + " enabled";
 }
+
+
+document.body.style.height = '200pt';
+
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = 'https://cdnjs.cloudflare.com/ajax/libs/cookie-banner/1.2.2/cookiebanner.min.js';
+script.id = 'cookiebanner';
+script['data-height']   = '20px'; // non-standard attribute (may need to use 'setAttribute(..)'
+script['data-position'] = 'top';
+script['data-message']  = 'We use cookies!';
+document.head.appendChild(script); 
+
+script.onload = function()
+{
+  console.log('loaded script');
+
+  // console.log( initCookieConsent );
+}
+console.log('ready');
